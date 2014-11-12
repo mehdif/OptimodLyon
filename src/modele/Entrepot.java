72bac4c9d533cea06a1abefa3b4904f602b17a52
vemad <vincent.emad@insa-lyon.fr>
2014-11-12 17:25:46 +0100

    Oublie de vueFenetre

--- src/modele/Entrepot.java
+++ src/modele/Entrepot.java
@@ -12,6 +12,18 @@ public class Entrepot extends Point {
     public Entrepot(Integer longitude, Integer latitude, Integer adresse) {
     	super(longitude, latitude, adresse);
     }
+
+	@Override
+	public Integer getLongitude() {
+		// TODO Auto-generated method stub
+		return super.getLongitude();
+	}
+
+	@Override
+	public Integer getLatitude() {
+		// TODO Auto-generated method stub
+		return super.getLatitude();
+	}
     
     
 
