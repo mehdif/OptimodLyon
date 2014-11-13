87a9247008b111350400b5e19a1e71479e8f085a
vemad <vincent.emad@insa-lyon.fr>
2014-11-13 20:27:33 +0100

    vue tournee

---
+++ src/vue/VueTournee.java
@@ -0,0 +1,42 @@
+package vue;
+
+import java.awt.Graphics;
+import java.util.*;
+
+/**
+ * @author Hexanome 4301
+ */
+public class VueTournee implements VueDessinable {
+
+    /**
+     * 
+     */
+    public VueTournee() {
+    	vuesItineraire = new ArrayList<VueItineraire>();
+    }
+
+    /**
+     * 
+     */
+    public List <VueItineraire> vuesItineraire;
+
+	@Override
+	public void dessiner(Graphics g) {
+
+		// TODO
+
+		for (VueItineraire uneVueItineraire : vuesItineraire) {
+			uneVueItineraire.dessiner(g);
+		};
+	}
+	
+    /**
+     * Methode ajoutant une vue itineraire a la vue tournee
+     */
+	public void ajouterVueItineraire(VueItineraire uneVueItineraire){
+		
+		this.vuesItineraire.add(uneVueItineraire);
+		
+	}
+
+}
